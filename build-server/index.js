6e797c51a0cd5aa65a5b9a3b88319003e3e13d75
anujkaushik1512 <anujkaushik1512@gmail.com>
2025-03-17 23:53:45 +0530

    recursively getting all files in subdir & concurrently runnng s3uploads

--- build-server/index.js
+++ build-server/index.js
@@ -1,8 +1,10 @@
 const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
-const { PutObjectCommand, S3Client, S3Client } = require("@aws-sdk/client-s3");
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 const mime = require("mime-types");
+
+require("dotenv").config();
 
 const s3Client = new S3Client({
   credentials: {
@@ -14,10 +16,53 @@ const s3Client = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID;
 
+function getAllFiles(_path) {
+  try {
+    let arrayOfFilePaths = [];
+    const pathFolderContent = fs.readdirSync(_path, { recursive: true });
+
+    for (const file of pathFolderContent) {
+      const filePath = path.join(_path, file);
+      if (fs.lstatSync(filePath).isDirectory()) {
+        const returnedFilePaths = getAllFiles(filePath);
+        arrayOfFilePaths = [...returnedFilePaths, ...arrayOfFilePaths];
+      } else {
+        arrayOfFilePaths.push(filePath);
+      }
+    }
+
+    return arrayOfFilePaths;
+  } catch (error) {
+    return error;
+  }
+}
+
+async function uploadFile(file) {
+  try {
+    const relativePath = file.split("/build/")[1] || file;
+
+    const command = new PutObjectCommand({
+      Bucket: "vercel-main",
+      Key: `__outputs/${PROJECT_ID}/${relativePath}`,
+      Body: fs.createReadStream(file),
+      ContentType: mime.lookup(file),
+    });
+
+    await s3Client.send(command);
+    console.log("completed");
+  } catch (error) {
+    console.log("error: ", error);
+
+    throw error;
+  }
+}
+
 async function main() {
   console.log("Executing script");
   const outputDirPath = path.join(__dirname, "output");
-  const process = exec(`cd ${outputDirPath} && npm install && npm run build`);
+  const process = exec(
+    `cd ${outputDirPath} && npm install --legacy-peer-deps  && npm run build`
+  );
 
   process.stdout.on("data", (logs) => console.log(logs.toString()));
   process.stderr.on("data", (data) => console.error("Error:", data.toString()));
@@ -27,30 +72,19 @@ async function main() {
 
   process.on("close", async () => {
     console.log("Build Complete");
-    const distFolderPath = path.join(outputDirPath, "dist");
-    const distFolderContents = fs.readdirSync(distFolderPath, {
-      recursive: true,
-    });
+    const distFolderPath = path.join(outputDirPath, "build");
 
-    const s3FileSendPromises = [];
-    for (const filePath of distFolderContents) {
-      if (fs.lstatSync(filePath).isDirectory()) continue;
-
-      const command = new PutObjectCommand({
-        Bucket: "vercel-main",
-        Key: `__outputs/${PROJECT_ID}`,
-        Body: fs.createReadStream(filePath),
-        ContentType: mime.lookup(path),
-      });
-
-      s3FileSendPromises.push(s3Client.send(command));
+    const files = getAllFiles(distFolderPath);
+    if (files instanceof Error) {
+      throw new Error(files.message);
     }
 
-    (await Promise.allSettled()).forEach((promise) =>
-      promise.status == "rejected"
-        ? console.log("File Uploading Failed: ", promise.reason)
-        : console.log("File Uploaded Successfully")
-    );
+    const s3FileSendPromises = [];
+    for (const file of files) {
+      s3FileSendPromises.push(uploadFile(file));
+    }
+
+    await Promise.allSettled(s3FileSendPromises);
   });
 }
 
--- build-server/main.sh
+++ build-server/main.sh
@@ -3,3 +3,10 @@
 export GIT_REPOSITORY_URL="$GIT_REPOSITORY_URL"
 git clone "$GIT_REPOSITORY_URL" /home/app/output
 exec node index.js
+
+
+# export GIT_REPOSITORY_URL="$GIT_REPOSITORY_URL"
+# git clone "$GIT_REPOSITORY_URL" output
+# exec node index.js
+
+
