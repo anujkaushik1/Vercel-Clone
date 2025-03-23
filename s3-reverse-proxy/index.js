c97375f9d463ebab336aa26f135e8f8aeaded35f
anujkaushik1512 <anujkaushik1512@gmail.com>
2025-03-23 17:59:59 +0530

    created dynamic reverse proxy

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const httpProxy = require("http-proxy");
+const proxy = httpProxy.createProxy();
+
+const PORT = 8000;
+
+const BASE_PATH = "https://vercel-main.s3.ap-south-1.amazonaws.com/__outputs/";
+
+app.use((req, res) => {
+  const hostName = req.hostname;
+  const subDomain = hostName.split(".")[0];
+  const resolvesTo = `${BASE_PATH}${subDomain}`;
+
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const path = req.url;
+  if (path === "/") {
+    proxyReq.path += "index.html";
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
